bdffa98ab361a3a26ec8bfe996881f9cc6d3a48b
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-04-23 23:23:05 +0200

    fix: compile issue

--- src/worldchat_loader.cpp
+++ src/worldchat_loader.cpp
@@ -18,7 +18,7 @@
 void AddSC_WorldChat();
 void AddSC_worldchat_commandscript();
 
-void Addmod_world_chatScripts()
+void Addmod_worldchatScripts()
 {
     AddSC_WorldChat();
     AddSC_worldchat_commandscript();
