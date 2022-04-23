14736a7a4a9b0814f91c8a1b7a7641c02bb74ca8
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-04-23 21:13:07 +0200

    feat: Add license headers

--- src/WorldChat.cpp
+++ src/WorldChat.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "WorldSession.h"
 #include "Channel.h"
 #include "GameTime.h"
@@ -274,17 +291,7 @@ void WorldChat::SendWorldChat(Player* player, const char* message)
     SessionMap sessions = sWorld->GetAllSessions();
     for (SessionMap::iterator itr = sessions.begin(); itr != sessions.end(); ++itr)
     {
-        if (!itr->second)
-        {
-            continue;
-        }
-
-        if (!itr->second->GetPlayer())
-        {
-            continue;
-        }
-
-        if (!itr->second->GetPlayer()->IsInWorld())
+        if (!itr->second || !itr->second->GetPlayer() || !itr->second->GetPlayer()->IsInWorld())
         {
             continue;
         }
